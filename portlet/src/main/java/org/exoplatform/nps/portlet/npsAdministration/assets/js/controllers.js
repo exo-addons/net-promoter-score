385ba904fb33bac3697e3ef8f66a368584315275
LamiaBouhajja <lbouhajja@exoplatform.com>
2018-02-28 14:39:44 +0100

    Date picker integration

--- portlet/src/main/java/org/exoplatform/nps/portlet/npsAdministration/assets/js/controllers.js
+++ portlet/src/main/java/org/exoplatform/nps/portlet/npsAdministration/assets/js/controllers.js
@@ -319,10 +319,9 @@ define("npsAdminControllers", ["SHARED/jquery", "SHARED/juzu-ajax"], function ($
             }).then(function successCallback(data) {
                 $scope.statNpScore = data.data;
                 var NPSArray = [];
+                var color = "#476a9c";
                 for (var i = 0; i < $scope.statNpScore.length; i++) {
                     var obj = $scope.statNpScore[i];
-                    var color = "#476a9c"
-
                     NPSArray.push({c: [{v: obj.score}, {v: obj.count}, {v: color}]});
                 }
 
--- portlet/src/main/java/org/exoplatform/nps/portlet/npsAdministration/assets/js/nps-admin.js
+++ portlet/src/main/java/org/exoplatform/nps/portlet/npsAdministration/assets/js/nps-admin.js
@@ -10,6 +10,41 @@ require( ["SHARED/jquery", "npsAdminControllers"], function ( $,  npsAdminContro
             console.log(e);
         }
 
+
+        /* DATE PICKER*/
+        var dateFormat = "dd/mm/yy",
+              from = $( "#fromDatepicker" )
+                .datepicker({
+                  changeMonth: true,
+                  changeYear: true,
+                  numberOfMonths: 2
+                })
+                .on( "change", function() {
+                  to.datepicker( "option", "minDate", getDate( this ) );
+                }),
+              to = $( "#toDatepicker" ).datepicker({
+                defaultDate: "+1w",
+                changeMonth: true,
+                changeYear: true,
+                numberOfMonths: 2
+              })
+              .on( "change", function() {
+                from.datepicker( "option", "maxDate", getDate( this ) );
+              });
+
+
+//            $( "#datepicker" ).datepicker( "option", "showAnim", "slideDown" );
+
     });
+    function getDate( element ) {
+          var date;
+          try {
+            date = $.datepicker.parseDate( dateFormat, element.value );
+          } catch( error ) {
+            date = null;
+          }
+
+          return date;
+    }
 
 });
